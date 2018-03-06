7a3b123a6d8e548383d558cd6ace1e5b3b610371
bardon20 <bardon20@up.edu>
2018-03-05 21:17:07 -0800

    commit

--- app/src/main/java/com/example/toshiba/bohnanzagamestate/MainActivity.java
+++ app/src/main/java/com/example/toshiba/bohnanzagamestate/MainActivity.java
@@ -4,18 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity  {
-    TextView testTextEdit;
+    EditText testTextEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testTextEdit = (TextView)findViewById(R.id.testText);
+        testTextEdit = (EditText) findViewById(R.id.testText);
         Button runTestButton = (Button)findViewById(R.id.testButton);
         runTestButton.setOnClickListener(new buttonListener());
     }
